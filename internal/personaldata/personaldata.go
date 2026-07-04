5779329ba325a972810bd5df8254f0e73a61bae0
Alexander Ionitsa <112870702+Alexskex@users.noreply.github.com>
2026-07-04 20:43:48 +0300

    5 commi

--- internal/personaldata/personaldata.go
+++ internal/personaldata/personaldata.go
@@ -9,8 +9,6 @@ type Personal struct {
 }
 
 func (p Personal) Print() {
-	fmt.Printf("Имя: %s\nВес: %.2f\nРост: %.2f \n", p.Namep, .Weightp, .Height)
-	
+	fmt.Printf("Имя: %s\nВес: %.2f\nРост: %.2f \n", p.Name, p.Weight, p.Height)
+
 }
-
-
